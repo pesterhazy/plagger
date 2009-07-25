c254c70a149eaaf27670c5706931493e6c61fa11
Paulus Esterhazy <pesterhazy@gmail.com>
2009-07-25 20:29:53 +0200

    Improved html detection, testing.

--- assets/plugins/Filter-EntryFullText/sueddeutsche_de.pl
+++ assets/plugins/Filter-EntryFullText/sueddeutsche_de.pl
@@ -1,4 +1,5 @@
 use HTML::Scrubber;
+use LWP::Simple;
 
 sub handle {
     my($self, $args) = @_;
@@ -7,31 +8,57 @@ sub handle {
 
 sub extract {
   my($self, $args) = @_;
-
-#my @lines = <>;
-#my $str = join('',@lines);
-
   my $str = $args->{content};
+  my $body;
 
-  if ($str =~ m#<div class="artikelheader clr">.*?(<div class="main content.*?)<table class="bgf2f2f2 absatz">#ms) {
-    print "match\n";
-    my $body = $1;
-
-    $body =~ s#<!-- Stoerer //-->.*?<!-- END Stoerer //-->##ms;
-    $body =~ s#<!-- performance media -->.*?<!-- //performance media -->##ms;
-    $body =~ s#<span>\s*Anzeige\s*</span>##msi;
-    $body =~ s#<p class="bannerAnzeige">ANZEIGE</p>##msi;
-    $body =~ s#<span class="bannerAnzeige">ANZEIGE</span>##msi;
-
-    my $scrubber = HTML::Scrubber->new( 
-      deny => [ qw[ div noscript iframe ] ],
-      default => 1,
-    );
-    $scrubber->rules(img => { "*" => 1 });
-    $body = $scrubber->scrub($body);
-    return($body);
+  if ($str =~ m#<div class="artikelheader clr">.*?(<div class="main content.*?)(?:<table class="bgf2f2f2 absatz">|<div class="artikelliste">|<div id="themenbox"|<div id="artikelfoot")#ms) {
+    $body = $1;
+  } elsif ($str =~ m#(<div class="main content.*?)(?:<table class="bgf2f2f2 absatz">|<div class="artikelliste">|<div id="themenbox"|<div id="artikelfoot")#ms) {
+    $body = $1;
   }
   else {
+    if (defined $args->{entry}) {
+      print STDERR "WARNING: Did not match: " . $args->{entry}->permalink . "\n";
+    } 
+    else {
+      print STDERR "WARNING: Did not match: unknown\n";
+    }
     return;
   }
+
+  $body =~ s#<!-- Stoerer //-->.*?<!-- END Stoerer //-->##ms;
+  $body =~ s#<!-- performance media -->.*?<!-- //performance media -->##ms;
+  $body =~ s#<span>\s*Anzeige\s*</span>##msi;
+  $body =~ s#<p class="bannerAnzeige">ANZEIGE</p>##msi;
+  $body =~ s#<span class="bannerAnzeige">ANZEIGE</span>##msi;
+
+  my $scrubber = HTML::Scrubber->new( 
+    deny => [ qw[ div noscript iframe ] ],
+    default => 1,
+  );
+  $scrubber->rules(img => { "*" => 1 });
+  $body = $scrubber->scrub($body);
+
+  if ($str =~ m#<!-- paging -->#ms) {
+    $body .= "<p>Mehr Seiten verfügbar...</p>\n";
+  }
+  return($body);
 }
+
+sub test {
+  my $str;
+
+  unless ($ARGV[0] eq '' ) {
+    $str = get($ARGV[0]);
+  }
+  else {
+    my @lines = <>;
+    $str = join('',@lines);
+  }
+  my $args = { content => $str };
+  my $body = extract(0, $args);
+
+  print $body;
+}
+
+test() unless (caller);
