5377d30642903e4f357c9c9790a97ed1dd5a8a0c
Paulus Esterhazy <pesterhazy@gmail.com>
2009-12-28 17:39:29 +0100

    remove debug statements

--- assets/plugins/Filter-EntryFullText/faz_net.pl
+++ assets/plugins/Filter-EntryFullText/faz_net.pl
@@ -12,60 +12,42 @@ sub extract {
   my $str = $args->{content};
   my $body;
 
-  print("Link: " . $args->{entry}->permalink . "\n");
-
   my $tree= HTML::TreeBuilder::XPath->new;
   $tree->parse($str);
 
+  my $nodes;
+  $nodes=$tree->findnodes('//div[@class="BlogPost"]');
+  if ( !$nodes ) {
+    $nodes=$tree->findnodes('//div[@class="Article"]');
+    if ( !$nodes ) {
+      $nodes=$tree->findnodes('//div[@id="MedContent"]');
+      if ( !$nodes ) {
+        print("WARNING: No such div found\n");
+        return;
+      }
+    }
+  }
+  my $anke = $nodes->get_node(1);
+
   my @to_delete = (
-    '//div[@id="ArtikelServicesMenu"]',
-    '//div[@class="BoxTool Aufklappen_Grau"]',
-    '//div[@class="DottedLineClear"]',
-    '//div[@class="DottedLine"]',
-    '//div[@class="ModulLesermeinungenFooter"]',
-    '//div[@class="ModulArtikelServices"]',
-    '//div[@style="padding:10px 0px; font-size:11px; line-height:16px; color:#7d7d7d"]',
-    '//div[@class="ModulVerlagsInfo"]',
+    './div[@id="ArtikelServicesMenu"]',
+    './div[@class="BoxTool Aufklappen_Grau"]',
+    './div[@class="DottedLineClear"]',
+    './div[@class="DottedLine"]',
+    './div[@class="ModulLesermeinungenFooter"]',
+    './div[@class="ModulArtikelServices"]',
+    './div[@style="padding:10px 0px; font-size:11px; line-height:16px; color:#7d7d7d"]',
+    './div[@class="ModulVerlagsInfo"]',
     );
 
   for my $xpath ( @to_delete ) {
-    print ("checking xpath $xpath...\n");
-    for my $node ( $tree->findnodes( $xpath ) ) {
-      print ("found node $node\n");
+    for my $node ( $anke->findnodes( $xpath ) ) {
       $node->detach;   # delete element
     }
   }
 
   my $body;
-  $body=$tree->findnodes_as_string('//div[@class="BlogPost"]');
-  if ( !$body ) {
-    $body=$tree->findnodes_as_string('//div[@class="Article"]');
-    if ( !$body ) {
-      $body=$tree->findnodes_as_string('//div[@id="MedContent"]');
-      if ( !$body ) {
-        die("No such div found");
-      }
-    }
-  }
-
-#  if ($str =~ m#<div class="artikelheader clr.*?(<div class="main content.*?)(?:<table class="bgf2f2f2 absatz">|<div class="artikelliste">|<div id="themenbox"|<div id="artikelfoot")#ms) {
-#    $body = $1;
-#  } elsif ($str =~ m#(<div class="main content.*?)(?:<table class="bgf2f2f2 absatz">|<div class="artikelliste">|<div id="themenbox"|<div id="artikelfoot")#ms) {
-#    $body = $1;
-#  }
-#  elsif ($str =~ m#(<div id="contentcolumn" class="entry-content" role="main">.*?)(?:<table class="bgf2f2f2 absatz">|<div class="artikelliste">|<div id="themenbox"|<div id="artikelfoot")#ms) {
-#    $body = $1;
-#  }
-#  else {
-#    if (defined $args->{entry}) {
-#      print STDERR "WARNING: Did not match: " . $args->{entry}->permalink . "\n";
-#    } 
-#    else {
-#      print STDERR "WARNING: Did not match: unknown\n";
-#    }
-#    return;
-#  }
-#  $body =~ s#<span class="bannerAnzeige">ANZEIGE</span>##msi;
+  $body=$anke->as_HTML();
 
   my $scrubber = HTML::Scrubber->new( 
     deny => [ qw[ div noscript iframe ] ],
@@ -90,7 +72,6 @@ sub test {
   my $args = { content => $str };
   my $body = extract(0, $args);
 
-  print $body;
 }
 
 test() unless (caller);
