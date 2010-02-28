2152244af4cae009b356e407245a108686ff5a9f
Paulus Esterhazy <pesterhazy@gmail.com>
2010-02-28 16:39:09 +0100

    improvements to FAZ

--- assets/plugins/Filter-EntryFullText/faz_net.pl
+++ assets/plugins/Filter-EntryFullText/faz_net.pl
@@ -1,6 +1,7 @@
 use HTML::Scrubber;
 use LWP::Simple;
 use HTML::TreeBuilder::XPath;
+use File::Slurp;
 
 sub handle {
     my($self, $args) = @_;
@@ -29,19 +30,30 @@ sub extract {
   }
   my $anke = $nodes->get_node(1);
 
+#  my $s=$anke->as_HTML();
+#  unless ( $s =~ /Konfuzius/i ) {
+#    print "Skipping\n";
+#    return;
+#  }
+#  print "found\n";
+#  write_file( "/tmp/in.html", $s);
+
   my @to_delete = (
     './div[@id="ArtikelServicesMenu"]',
     './div[@class="BoxTool Aufklappen_Grau"]',
     './div[@class="DottedLineClear"]',
     './div[@class="DottedLine"]',
     './div[@class="ModulLesermeinungenFooter"]',
-    './div[@class="ModulArtikelServices"]',
+    './/div[@class="ModulArtikelServices"]',
     './div[@style="padding:10px 0px; font-size:11px; line-height:16px; color:#7d7d7d"]',
     './div[@class="ModulVerlagsInfo"]',
+    './div[@class="LinkBoxModulSmall"]',
+    './/div[@id="MedRight"]',
     );
 
   for my $xpath ( @to_delete ) {
     for my $node ( $anke->findnodes( $xpath ) ) {
+#      print "hit $xpath\n";
       $node->detach;   # delete element
     }
   }
@@ -53,8 +65,13 @@ sub extract {
     deny => [ qw[ div noscript iframe ] ],
     default => 1,
   );
-  $scrubber->rules(img => { "*" => 1 });
+  $scrubber->rules(img => { "*" => 1 }, p => { "*" => 1 });
   $body = $scrubber->scrub($body);
+
+  $body =~ s/&#132;/&bdquo;/g;
+
+#  write_file("/tmp/out.html",$body);
+#  print($body);
 
   return($body);
 }
